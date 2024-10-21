77e88e406b6330f7c396c2d1050af3298a2c0068
Sipho Mzayiya <realsiphomzayiya@gmail.com>
2024-10-21 23:07:26 +0200

    hello flutter app

--- helloflutter/lib/main.dart
+++ helloflutter/lib/main.dart
@@ -1,12 +1,18 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
-    home: Scaffold(
-      appBar: AppBar(
-        title: Text("Hello Flutter!"),
-      ),
-      body: Text("Hello Flutter"),
-    ),
-  ));
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+          appBar: AppBar(
+            title: Text("Hello Flutter!"),
+          ),
+          body: Container(height: 100, width: 100, color: Colors.indigo)),
+    );
+  }
 }
