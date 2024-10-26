ba7745ef6da8ad3a11115350455a8987896ca94c
Sipho Mzayiya <realsiphomzayiya@gmail.com>
2024-10-26 16:03:27 +0200

    creating home app.

--- billionaireapp/lib/main.dart
+++ billionaireapp/lib/main.dart
@@ -7,6 +7,10 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
+  void aFunction() {
+    print("Button is pressed!");
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -17,11 +21,20 @@ class MyApp extends StatelessWidget {
           title: Text("Billionaire App"),
         ),
         body: Container(
+          padding: EdgeInsets.all(20),
           color: Colors.blueGrey[700],
           height: double.infinity,
           width: double.infinity,
           child: Column(
-            children: [Text("Balance part"), Text("Button part")],
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Row(children: [
+                Container(color: Colors.red,
+                child: Text("1"),)
+              ],)
+              // Text("Balance part"),
+              ElevatedButton(onPressed: aFunction, child: Text("Clicke here!"))
+            ],
           ),
         ),
       ),
