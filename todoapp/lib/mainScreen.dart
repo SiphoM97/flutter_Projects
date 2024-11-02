3c14eaf780243eb9e7e9dc325be9310a9e142d0e
Sipho Mzayiya <realsiphomzayiya@gmail.com>
2024-11-02 11:43:32 +0200

    updated main screen

--- todoapp/lib/mainScreen.dart
+++ todoapp/lib/mainScreen.dart
@@ -10,6 +10,6 @@ class Mainscreen extends StatefulWidget {
 class _MainscreenState extends State<Mainscreen> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold();
   }
 }
