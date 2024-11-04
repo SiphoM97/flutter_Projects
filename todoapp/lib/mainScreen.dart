39198ec67d65d83f1daf2e4e3749c605e0977683
Sipho Mzayiya <realsiphomzayiya@gmail.com>
2024-11-04 19:51:45 +0200

    Added flutter feather icon depandancies.

--- todoapp/lib/mainScreen.dart
+++ todoapp/lib/mainScreen.dart
@@ -18,6 +18,7 @@ class _MainscreenState extends State<Mainscreen> {
           Icon(
             Icons.add,
             color: Colors.red,
+            size: 45,
           )
         ],
       ),
