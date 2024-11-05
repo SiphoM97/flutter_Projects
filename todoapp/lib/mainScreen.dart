1ea62612a5ab4cb00f1c130f6fccffd19e06ab45
Sipho Mzayiya <realsiphomzayiya@gmail.com>
2024-11-05 12:16:49 +0200

    morning final update

--- todoapp/lib/mainScreen.dart
+++ todoapp/lib/mainScreen.dart
@@ -20,41 +20,42 @@ class _MainscreenState extends State<Mainscreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      drawer: Drawer(
-        child: Text("Drawer data"),
-      ),
-      appBar: AppBar(
-        centerTitle: true,
-        title: Text("ToDo App"),
-        actions: [
-          InkWell(
-            splashColor: Colors.blue,
-            onTap: () {
-              showModalBottomSheet(
-                  context: context,
-                  builder: (context) {
-                    return Padding(
-                      padding: MediaQuery.of(context).viewInsets,
-                      child: Container(
-                        padding: EdgeInsets.all(20),
-                        height: 200,
-                        child: AddToDo(changeText: changeText),
-                      ),
-                    );
-                  });
-            },
-            child: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Icon(
-                Icons.add,
+        drawer: Drawer(
+          child: Text("Drawer data"),
+        ),
+        appBar: AppBar(
+          centerTitle: true,
+          title: Text("ToDo App"),
+          actions: [
+            InkWell(
+              splashColor: Colors.blue,
+              onTap: () {
+                showModalBottomSheet(
+                    context: context,
+                    builder: (context) {
+                      return Padding(
+                        padding: MediaQuery.of(context).viewInsets,
+                        child: Container(
+                          padding: EdgeInsets.all(20),
+                          height: 200,
+                          child: AddToDo(changeText: changeText),
+                        ),
+                      );
+                    });
+              },
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Icon(
+                  Icons.add,
+                ),
               ),
-            ),
-          )
-        ],
-      ),
-      body: Container(
-        child: Text("$text"),
-      ),
-    );
+            )
+          ],
+        ),
+        body: ListView.builder(
+            itemCount: toDoList.length,
+            itemBuilder: (BuildContext context, int index) {
+              return Text(toDoList[index]);
+            }));
   }
 }
