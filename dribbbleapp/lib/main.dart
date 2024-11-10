07abf53b4df707156a9c9451bce27e5591f79c01
Sipho Mzayiya <realsiphomzayiya@gmail.com>
2024-11-10 12:06:33 +0200

    planning the App.

--- dribbbleapp/lib/main.dart
+++ dribbbleapp/lib/main.dart
@@ -19,6 +19,17 @@ class MyApp extends StatelessWidget {
             style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
           ),
         ),
+        body: Container(
+          padding: EdgeInsets.all(20),
+          margin: EdgeInsets.only(
+            top: 10,
+            left: 10,
+          ),
+          child: Text("Test Text!"),
+          height: 100,
+          width: 100,
+          color: Colors.amber,
+        ),
       ),
     );
   }
