70598211649954102d23211b88b2949d6d14697d
Sipho Mzayiya <realsiphomzayiya@gmail.com>
2024-11-11 23:00:50 +0200

    deleted container

--- dribbbleapp/lib/main.dart
+++ dribbbleapp/lib/main.dart
@@ -38,18 +38,6 @@ class MyApp extends StatelessWidget {
               padding: EdgeInsets.all(20),
               margin: EdgeInsets.all(10),
               child: Text(
-                "Meal two",
-                style: TextStyle(color: Colors.white),
-              ),
-              height: 150,
-              width: 150,
-              decoration: BoxDecoration(
-                  color: Colors.black, borderRadius: BorderRadius.circular(10)),
-            ),
-            Container(
-              padding: EdgeInsets.all(20),
-              margin: EdgeInsets.all(10),
-              child: Text(
                 "Meal three",
                 style: TextStyle(color: Colors.white),
               ),
