483d52a366c6a6cedef7cfaa763b1e287f6eedb8
Sipho Mzayiya <realsiphomzayiya@gmail.com>
2024-12-12 07:22:03 +0200

    first emulator run.

--- bucketlist/lib/main.dart
+++ bucketlist/lib/main.dart
@@ -16,7 +16,7 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-        theme: ThemeData.light(useMaterial3: true), home: Mainscreen());
+    return MaterialApp(
+        theme: ThemeData.light(useMaterial3: true), home: const Mainscreen());
   }
 }
