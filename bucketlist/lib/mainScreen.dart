912a518ece13d3983aa004b2366e3d49181ba58e
Sipho Mzayiya <realsiphomzayiya@gmail.com>
2024-12-16 13:32:58 +0200

    added loading text

--- bucketlist/lib/mainScreen.dart
+++ bucketlist/lib/mainScreen.dart
@@ -57,23 +57,25 @@ class _MainscreenState extends State<Mainscreen> {
         onRefresh: () async {
           getData();
         },
-        child: ListView.builder(
-            itemCount: bucketListData.length,
-            itemBuilder: (BuildContext context, int index) {
-              return Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: ListTile(
-                  leading: CircleAvatar(
-                    radius: 20,
-                    backgroundImage:
-                        NetworkImage(bucketListData[index]['Image'] ?? ""),
-                  ),
-                  title: Text(bucketListData[index]['Item'] ?? ""),
-                  trailing:
-                      Text(bucketListData[index]['cost'].toString() ?? ""),
-                ),
-              );
-            }),
+        child: isLoading
+            ? Text("Loading")
+            : ListView.builder(
+                itemCount: bucketListData.length,
+                itemBuilder: (BuildContext context, int index) {
+                  return Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: ListTile(
+                      leading: CircleAvatar(
+                        radius: 20,
+                        backgroundImage:
+                            NetworkImage(bucketListData[index]['Image'] ?? ""),
+                      ),
+                      title: Text(bucketListData[index]['Item'] ?? ""),
+                      trailing:
+                          Text(bucketListData[index]['cost'].toString() ?? ""),
+                    ),
+                  );
+                }),
       ),
     );
   }
